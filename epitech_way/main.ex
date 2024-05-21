001691883d26ab27af50fe53be8790f92a454b2a
DURAND Malo <malo.durand@epitech.eu>
2024-05-21 02:15:58 +0200

    feat: adding few tasks from day04

--- epitech_way/main.ex
+++ epitech_way/main.ex
@@ -1,11 +1,12 @@
-# Sources
-# https://hexdocs.pm/elixir/comprehensions.html
-# https://stackoverflow.com/questions/41313995/elixir-convert-integer-to-unicode-character
-# https://elixirforum.com/t/multiple-guards-in-elixir/11327/3
-# https://github.com/elixir-lang/elixir/blob/v1.6/CHANGELOG.md#defguard-and-defguardp
-# Guards in Elixir : https://www.youtube.com/watch?v=fJbL7YrHrUA
-
 defmodule Day03 do
+  # Resources
+  # https://hexdocs.pm/elixir/comprehensions.html
+  # https://stackoverflow.com/questions/41313995/elixir-convert-integer-to-unicode-character
+  # https://elixirforum.com/t/multiple-guards-in-elixir/11327/3
+  # https://github.com/elixir-lang/elixir/blob/v1.6/CHANGELOG.md#defguard-and-defguardp
+  # Guards in Elixir : https://www.youtube.com/watch?v=fJbL7YrHrUA
+  # https://stackoverflow.com/questions/66681743/how-to-check-if-the-elements-of-an-array-are-in-sequential-order-in-elixir
+
   defp my_print_comb_printable(n, s \\ 3)
   when is_integer(n) and is_integer(s) and s > 0 do
     digits = Integer.digits(n)
@@ -45,4 +46,30 @@ defmodule Day03 do
       IO.puts(i)
     end
   end
+end
+
+defmodule Day04 do
+  # Resources
+  # https://elixirforum.com/t/find-the-first-recurring-character-in-a-string/13717/2
+  # https://stackoverflow.com/questions/25896762/how-can-pattern-matching-be-done-on-text
+
+  def my_swap({a, b}) do
+    {b, a}
+  end
+
+  def my_strlen(s) when is_bitstring(s) do
+    Enum.reduce_while(String.graphemes(s), 0, fn _, acc -> { :cont, acc + 1 } end)
+  end
+
+  def my_evil_str(s) when is_bitstring(s) do
+    Enum.reduce_while(String.graphemes(s), "", fn c, acc -> { :cont, c <> acc} end)
+  end
+
+  # defp get_sign(s) when is_bitstring(s) do
+  #   { 1, s }
+  # end
+
+  # def my_getnbr(s) when is_bitstring(s) do
+  #   { sign, rest } = get_sign(s)
+  # end
 end
